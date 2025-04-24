69fa7437f457394862509d8a33714af4fcd7008c
gcolclough <10335751+gcolclough@users.noreply.github.com>
2025-04-23 21:51:00 -0400

    now trying to use AzureWebJobsStorage__accountName

--- src/functions/getTransactions.js
+++ src/functions/getTransactions.js
@@ -4,7 +4,7 @@ const { app, input} = require('@azure/functions');
 
 const blobInput = input.storageBlob({
     path: 'auth/cedar-example.json',
-    connection: 'AzureWebJobsStorage__connectionString', 
+    connection: 'AzureWebJobsStorage__accountName', 
 });
 
 
